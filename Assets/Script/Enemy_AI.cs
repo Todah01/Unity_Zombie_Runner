a082460a4b3a4400c0417a6e0d3b1980558de2ac
Todah01 <wobo5695@gmail.com>
2023-10-14 20:14:31 +0900

    Fix bug from enemy target system
    set distance between target and enemy from navmeshagent to improve fidelity of enemy targeting goal.

--- Assets/Script/Enemy_AI.cs
+++ Assets/Script/Enemy_AI.cs
@@ -7,13 +7,21 @@ using UnityEngine.AI;
 public class Enemy_AI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
 
     private void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, this.transform.position);
+        if(distanceToTarget <= chaseRange){
+            navMeshAgent.SetDestination(target.position);
+        }
+        else{
+            
+        }
     }
 }
